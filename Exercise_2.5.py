48306d1297ace3b45b0b5f9d13439425f6872ccd
EiderDM <89048753+EiderDM@users.noreply.github.com>
2021-08-18 16:55:49 -0500

    Update Exercise_2.5.py

--- Exercise_2.5.py
+++ Exercise_2.5.py
@@ -7,13 +7,20 @@ program to compute and print out the transmission and reflection probabilities u
 # load numpy module and pyplot from matplotlib library
 import numpy as np
 from matplotlib import pyplot as plt
+from math import * #import all functions and variables from the package math
 #Defining variables
-m=9,11*10**-31
+m=9.11*(10**-31)
 E=10
 V=9
-h=6,582119569*10**-16
+h=6.582119569*(10**-16)
+k1=(sqrt(2*m*E))/h
+k2=(sqrt(2*m*(E-V)))/h
+T=(4*k1*k2)/((k1+k2)**2)
+R=((k1-k2)/(k1+k2))**2
+print("The value of T is: ",T)
+print("The value of R is: ",R)
 
-
-
-
-
+if T>R:
+ print("Therefore, the particle pass the step")
+else:
+  print("Therefore, the particle don´t pass the step")
