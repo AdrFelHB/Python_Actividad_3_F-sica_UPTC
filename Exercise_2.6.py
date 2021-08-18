c05ac4b83106c044f6cab66fe8e2794ca6c7ed87
EiderDM <89048753+EiderDM@users.noreply.github.com>
2021-08-18 17:07:24 -0500

    Update Exercise_2.6.py

--- Exercise_2.6.py
+++ Exercise_2.6.py
@@ -1,1 +1,13 @@
+"""
+The orbit in space of one body around another, such as a planet around the Sun, need
+not be circular. In general it takes the formof an ellipse, with the body sometimes closer
+in and sometimes further out. If you are given the distance ℓ1 of closest approach that a
+planet makes to the Sun, also called its perihelion, and its linear velocity v1 at perihelion,
+then any other property of the orbit can be calculated from these two as follows.
+   a) Kepler’s second law tells us that the distance ℓ2 and velocity v2 of the planet at
+   its most distant point, or aphelion, satisfy ℓ2v2 = ℓ1v1. At the same time the total
+   energy, kinetic plus gravitational, of a planet with velocity v and distance r from
+   the Sun is given by...
+   
 
+"""
