95d7d1c9c0db270e786988148d7e35cba2d5687c
EiderDM <89048753+EiderDM@users.noreply.github.com>
2021-08-18 16:38:53 -0500

    Update and rename Exercise_2.4.py to Exercise_2.5.py

---
+++ Exercise_2.5.py
@@ -0,0 +1,19 @@
+"""
+Exercise 2.5 Quantum potential step: Suppose we have a particle with mass equal to the electron mass m = 9.11 ×
+10−31 kg and energy 10 eV encountering a potential step of height 9 eV. Write a Python 
+program to compute and print out the transmission and reflection probabilities using the formulas above.
+"""
+
+# load numpy module and pyplot from matplotlib library
+import numpy as np
+from matplotlib import pyplot as plt
+#Defining variables
+m=9,11*10**-31
+E=10
+V=9
+h=6,582119569*10**-16
+
+
+
+
+
