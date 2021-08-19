6f24d3476c6d5cce317525ca8d2840477cd99979
EiderDM <89048753+EiderDM@users.noreply.github.com>
2021-08-18 23:36:06 -0500

    Update Exercise_2.5.py

--- Exercise_2.5.py
+++ Exercise_2.5.py
@@ -1,6 +1,6 @@
 """
 Exercise 2.5 Quantum potential step: Suppose we have a particle with mass equal to the electron mass m = 9.11 ×
-10−31 kg and energy 10 eV encountering a potential step of height 9 eV. Write a Python 
+10−31 kg and energy 10 eV encountering a potential step of height 9 eV. Write a Python
 program to compute and print out the transmission and reflection probabilities using the formulas above.
 """
 
@@ -12,25 +12,18 @@ from math import * #import all functions and variables from the package math
 m=float(input("Enter the mass of the particle in MeV/c^2: "))
 E=float(input("Enter the initial kinetic energy E of the particle in eV: "))
 V=float(input("Enter the potential energy V of the particle in eV: "))
-
-while E <= V:
- Print("The potential V is greater than the initial energy E and, therefore, the particle never will pass the step\n")
- E=float(input("Enter the initial kinetic energy E of the particle in eV: "))
- V=float(input("Enter the potential energy V of the particle in eV: "))
-"""
 if E<V:
- 
-else:
-"""
-h=6.582119569*(10**-16)
-k1=(sqrt(2*m*E))/h
-k2=(sqrt(2*m*(E-V)))/h
-T=(4*k1*k2)/((k1+k2)**2)
-R=((k1-k2)/(k1+k2))**2
-print("The value of T is: ",T)
-print("The value of R is: ",R)
+ print("The potential V is greater than the initial energy E and, therefore, the particle never will pass the step")
+elif E>V:
+ h=6.582119569*(10**-16)
+ k1=(sqrt(2*m*E))/h
+ k2=(sqrt(2*m*(E-V)))/h
+ T=(4*k1*k2)/((k1+k2)**2)
+ R=((k1-k2)/(k1+k2))**2
+ print("The value of T is: ",T)
+ print("The value of R is: ",R)
 
-if T>R:
- print("Therefore, the particle pass the step")
-else:
+ if T>R:
+  print("Therefore, the particle pass the step")
+ else:
   print("Therefore, the particle don´t pass the step")
