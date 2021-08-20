af6ba642761c60c07bfbad514b2426ae8ad1642c
AdrFelHB <89048321+AdrFelHB@users.noreply.github.com>
2021-08-19 19:59:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,19 +7,17 @@ Autores:    Adrián Felipe Hernández Borda
 """
 
 """
-spaceship travels from Earth in a straight line at relativistic speed v to
-another planet x light years away. Write a program to ask the user for the value of x
-and the speed v as a fraction of the speed of light c, then print out the time in years that
-the spaceship takes to reach its destination (a) in the rest frame of an observer on Earth
-and (b) as perceived by a passenger on board the ship. Use your program to calculate
-the answers for a planet 10 light years away with v 0.99c.
-"""
-"""
 Exercise:   2.4
 Purpose:    Compute the time interval respect to earth and respect to a spaceship
             required by a spaceship that travels with velocity v from earth to any
             distant planet x lightyears away
-            
+Statement
+            spaceship travels from Earth in a straight line at relativistic speed v to
+            another planet x light years away. Write a program to ask the user for the value of x
+            and the speed v as a fraction of the speed of light c, then print out the time in years that
+            the spaceship takes to reach its destination (a) in the rest frame of an observer on Earth
+            and (b) as perceived by a passenger on board the ship. Use your program to calculate
+            the answers for a planet 10 light years away with v 0.99c.
 """
 # load numpy module and pyplot from matplotlib library
 import numpy as np
@@ -38,7 +36,7 @@ def compute_time(x,v):
     frame_menu = True
     
     while frame_menu:
-        print("Select the reference frame\n (1) Earth\n (2) Spaceship\n (3) exit")
+        print("Select the reference frame\n (1) Earth\n (2) Spaceship")
         selection = int(input())
 
         #   compute time for both of reference frames
@@ -142,15 +140,18 @@ plot_time(x,v,t_earth,t_spaceship)
 
 
 """
-The orbit in space of one body around another, such as a planet around the Sun, need
-not be circular. In general it takes the formof an ellipse, with the body sometimes closer
-in and sometimes further out. If you are given the distance ℓ1 of closest approach that a
-planet makes to the Sun, also called its perihelion, and its linear velocity v1 at perihelion,
-then any other property of the orbit can be calculated from these two as follows.
-   a) Kepler’s second law tells us that the distance ℓ2 and velocity v2 of the planet at
-   its most distant point, or aphelion, satisfy ℓ2v2 = ℓ1v1. At the same time the total
-   energy, kinetic plus gravitational, of a planet with velocity v and distance r from
-   the Sun is given by...
+Exercise:   2.6
+Statement:
+
+            The orbit in space of one body around another, such as a planet around the Sun, need
+            not be circular. In general it takes the formof an ellipse, with the body sometimes closer
+            in and sometimes further out. If you are given the distance ℓ1 of closest approach that a
+            planet makes to the Sun, also called its perihelion, and its linear velocity v1 at perihelion,
+            then any other property of the orbit can be calculated from these two as follows.
+               a) Kepler’s second law tells us that the distance ℓ2 and velocity v2 of the planet at
+               its most distant point, or aphelion, satisfy ℓ2v2 = ℓ1v1. At the same time the total
+               energy, kinetic plus gravitational, of a planet with velocity v and distance r from
+               the Sun is given by...
    
 for Earth
 Lp:1.4710e11 #m
